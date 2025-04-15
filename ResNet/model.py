a5d2c6727e791220ceee8020d937e6236fca45f2
Rain <daigang344@163.com>
2025-04-15 15:36:34 +0800

    add resnet

--- ResNet/model.py
+++ ResNet/model.py
@@ -1,1 +1,70 @@
 # author xiaogang
+import torch
+from torch import nn
+from torchsummary import summary
+
+
+class Resiual(nn.Module):
+    def __init__(self, in_channels, num_channels,use_1conv=False,stride=1):
+        super().__init__()
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(in_channels=in_channels,out_channels=num_channels,kernel_size=3,padding=1,stride=stride)
+        self.conv2 = nn.Conv2d(in_channels=num_channels,out_channels=num_channels,kernel_size=3,padding=1)
+        self.bn1 = nn.BatchNorm2d(num_channels)
+        self.bn2 = nn.BatchNorm2d(num_channels)
+        if use_1conv:
+            self.conv3 = nn.Conv2d(in_channels=in_channels,out_channels=num_channels,kernel_size=1,stride=stride)
+        else:
+            self.conv3 = None
+    def forward(self, x):
+        y = self.relu(self.bn1(self.conv1(x)))
+        y = self.bn2(self.conv2(y))
+        if self.conv3 is not None:
+            x = self.conv3(x)
+        y = self.relu(x + y)
+        return y
+
+class ResNet(nn.Module):
+    def __init__(self, r):
+        super().__init__()
+        self.b1 = nn.Sequential(
+            nn.Conv2d(in_channels=1,out_channels=64,kernel_size=7,padding=3,stride=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=3, stride=2,padding=1),
+        )
+        self.b2 = nn.Sequential(
+            r(64, 64, use_1conv=False,stride=1),
+            r(64, 64, use_1conv=False, stride=1),
+        )
+        self.b3 = nn.Sequential(
+            r(64, 128, use_1conv=True,stride=2),
+            r(128, 128, use_1conv=False, stride=1),
+        )
+        self.b4 = nn.Sequential(
+            r(128, 256, use_1conv=True,stride=2),
+            r(256, 256, use_1conv=False, stride=1),
+        )
+        self.b5 = nn.Sequential(
+            r(256, 512, use_1conv=True,stride=2),
+            r(512, 512, use_1conv=False, stride=1),
+        )
+        self.b6 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+            nn.Linear(in_features=512, out_features=10),
+        )
+    def forward(self, x):
+        y = self.b1(x)
+        y = self.b2(y)
+        y = self.b3(y)
+        y = self.b4(y)
+        y = self.b5(y)
+        y = self.b6(y)
+        return y
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = ResNet(Resiual).to(device)
+    print(summary(model,(1,224,224)))
+
--- ResNet/model_test.py
+++ ResNet/model_test.py
@@ -1,1 +1,64 @@
 # author xiaogang
+# author xiaogang
+
+import torch
+from torchvision.datasets import FashionMNIST
+from torchvision import transforms
+from torch.utils.data import DataLoader
+# from Vgg.model import VGG16
+# from GoogLeNet.model import GoogLeNet,Inception
+
+from ResNet.model import *
+def test_VGG_data_process():
+    test_set = FashionMNIST(root='../dataset',
+                              train=False,
+                              download=True,
+                              transform=transforms.Compose([transforms.ToTensor(),transforms.Resize(227)]))
+    # train_data,verify_data = random_split(train_set,[round(0.8*len(train_set)),round(0.2*len(train_set))])
+    test_loder = DataLoader(test_set,batch_size=1,shuffle=True,num_workers=4)
+    # verify_loder = DataLoader(verify_data,batch_size=64,shuffle=True,num_workers=4)
+    return test_loder
+
+def test_model(model,test_dataloader):
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = model.to(device)
+    test_correct = 0
+    test_num = 0
+    with torch.no_grad():
+        for data,label in test_dataloader:
+            data,label = data.to(device),label.to(device)
+            model.eval()
+            output = model(data)
+            pre_lab = torch.argmax(output,dim=1)
+            test_correct += torch.sum(pre_lab == label.data)
+            test_num += data.size(0)
+    test_acc = test_correct.double().item() / test_num
+    print("Test Accuracy: {:.3f}".format(test_acc))
+
+def test_value_model(model,test_dataloader):
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = model.to(device)
+
+    classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    with torch.no_grad():
+        for data,label in test_dataloader:
+            data,label = data.to(device),label.to(device)
+            model.eval()
+            output = model(data)
+            pre_lab = torch.argmax(output,dim=1)
+            result = pre_lab.item()
+            really = label.item()
+            print('预测值',classes[result],'真实值',classes[really])
+            # print('预测值',result,'真实值',really)
+
+
+
+if __name__ == '__main__':
+    net = ResNet(Resiual)
+
+    net.load_state_dict(torch.load('./save_model/model.pth'))
+    test_loder = test_VGG_data_process()
+    test_model(net,test_loder)
+    # test_value_model(net,test_loder)
